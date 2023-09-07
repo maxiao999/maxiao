d05eb92b9e66e5aeedfb66999422d34d4d6c6868
maxiao <1026197051@qq.com>
2023-09-07 23:45:44 +0800

    自动推送

---
+++ push.sh
@@ -0,0 +1,16 @@
+git config --global user.name 'maxiao'
+git config --global user.email 'maxiao999'
+git config --global http.sslverify false
+git add .自动推送
+git commit -m $1
+git push
+        
+    git config --global user.name 'maxiao'
+git config --global user.email '1026197051@qq.com'
+git config --global http.sslverify false
+git add .
+git commit -m '自动推送'
+git push
+push.sh
+ push.sh
+ 
